b87b611e189d0460601f59478e98697373169a44
umano <concon.wolf@gmail.com>
2019-10-05 21:20:37 +0900

    goodbye

--- old2/neural_network.py
+++
@@ -1,8 +0,0 @@
-from .layer import Layer
-class NeuralNetwork(Layer):
-    def __init__(self, *args, **kwargs):
-        pass
-    def fp(self, x, *_, **__kwargs):
-        pass
-    def add_layer():
-
